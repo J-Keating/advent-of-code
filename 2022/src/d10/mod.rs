d4b9ae8df65acf4015238b9a7ebaf17422b2d67f
Jonas Keating <jonask@microsoft.com>
2022-12-10 17:39:59 -0800

    Day 10.

--- 2022/src/d10/mod.rs
+++ 2022/src/d10/mod.rs
@@ -1,88 +1,93 @@
-use std::{fs, collections::HashMap, collections::HashSet };
+use std::{fs};
 
 use ::function_name::named;
 use itertools::Itertools;
-use lazy_static::lazy_static;
-
-#[derive(Eq, Hash, PartialEq, Clone, Copy)]
-struct Point {
-    pub x: i32,
-    pub y: i32
-}
-
-impl Point {
-    fn get_direction(dir_str: &str) -> Point {
-        lazy_static! {
-            static ref DIRECTION_MAP: HashMap<&'static str, Point> = HashMap::from([
-                ("U", Point { x:0, y:1 } ),
-                ("D", Point { x:0, y:-1 } ),
-                ("L", Point { x:-1, y:0 } ),
-                ("R", Point { x:1, y:0 } ),
-            ]);
-        }
-        
-        DIRECTION_MAP.get(dir_str).unwrap().clone( )
-    }
-
-    fn move_by(&mut self, other: &Point) {
-        self.x += other.x;
-        self.y += other.y;
-    }
-
-    fn move_toward(&mut self, other: &Point) {
-        let (x, y) = (other.x - self.x, other.y - self.y);
-        if num::abs(x) > 1 || num::abs(y) > 1 {
-            let step = Point { x: num::clamp(x, -1, 1), y: num::clamp(y, -1, 1) };
-            self.move_by(&step);
-        }
-    }
-}
+//use lazy_static::lazy_static;
 
 #[named]
 fn part1() {
     let file_contents = fs::read_to_string("src\\d10\\data.txt").expect("Error loading file");
-    let file_lines = file_contents.split("\r\n").collect::<Vec<&str>>();
+    let mut file_lines = file_contents.split("\r\n");
 
-    let mut head = Point { x: 0, y: 0 };
-    let mut tail = Point { x: 0, y: 0 };
-    let mut visited: HashSet<Point> = HashSet::new();
+    let mut iteration_count = 0;
+    let mut register = 1;
+    let mut pending_write: Option<i32> = None;
+    let mut total_strength = 0;
 
-    for line in file_lines {
-        let mut tokens = line.split(' ');
-        let dir = Point::get_direction(tokens.next().unwrap());
-        let num = tokens.next().unwrap().parse::<i32>().unwrap();
-        for _ in 0..num {
-            head.move_by(&dir);
-            tail.move_toward(&head);
-            visited.insert(tail);
+    loop {
+        iteration_count += 1;
+
+        // Use Register here
+        //println!("{}: {} ({:?})", iteration_count, register, pending_write);
+        if (iteration_count - 20) % 40 == 0 {
+            total_strength += iteration_count * register;
+        }
+
+        if pending_write.is_some() {
+            register = pending_write.unwrap();
+            pending_write = None;
+            continue;
+        }
+
+        let next_line = file_lines.next();
+        if next_line.is_none() {
+            break;
+        }
+        
+        let instruction = next_line.unwrap().split(' ').collect::<Vec<&str>>();
+        assert!((instruction.len() == 1 && instruction[0] == "noop") || (instruction.len() == 2 && instruction[0] == "addx"));
+        match instruction[0] {
+            "noop" => { }
+            "addx" => {
+                pending_write = Some(register + instruction[1].parse::<i32>().unwrap());
+            }
+            _ => { panic!() }
         }
     }
-
-    println!("{}: {}", function_name!(), visited.len());
+    println!("{}: {}", function_name!(), total_strength);
 }
 
 #[named]
 fn part2() {
     let file_contents = fs::read_to_string("src\\d10\\data.txt").expect("Error loading file");
-    let file_lines = file_contents.split("\r\n").collect::<Vec<&str>>();
+    let mut file_lines = file_contents.split("\r\n");
 
-    let mut snake: [Point; 10] = [ Point { x:0, y:0 }; 10];
-    let mut visited: HashSet<Point> = HashSet::new();
+    let mut iteration_count = 0;
+    let mut register = 1;
+    let mut pending_write: Option<i32> = None;
+    
+    println!("{}:", function_name!());
+    loop {
+        iteration_count += 1;
+        let crt_index = (iteration_count - 1) % 40;
 
-    for line in file_lines {
-        let (dir_str, num_str) = line.split(' ').collect_tuple().unwrap();
-        let dir = Point::get_direction(dir_str);
-        let num = num_str.parse::<i32>().unwrap();
-        for _ in 0..num {
-            snake[0].move_by(&dir);
-            for i in 1..10 {
-                snake[i].move_toward(&snake[i-1].clone());
+        print!("{}", if num::abs(crt_index - register) < 2 { '#' } else { '.'});
+
+        if iteration_count % 40 == 0 {
+            println!("");
+        }
+
+        if pending_write.is_some() {
+            register = pending_write.unwrap();
+            pending_write = None;
+            continue;
+        }
+
+        let next_line = file_lines.next();
+        if next_line.is_none() {
+            break;
+        }
+
+        let instruction = next_line.unwrap().split(' ').collect::<Vec<&str>>();
+        assert!((instruction.len() == 1 && instruction[0] == "noop") || (instruction.len() == 2 && instruction[0] == "addx"));
+        match instruction[0] {
+            "noop" => { }
+            "addx" => {
+                pending_write = Some(register + instruction[1].parse::<i32>().unwrap());
             }
-            visited.insert(snake[9]);
+            _ => { panic!() }
         }
     }
-
-    println!("{}: {}", function_name!(), visited.len());
 }
 
 pub fn run() {
@@ -90,5 +95,11 @@ pub fn run() {
     part2();
 }
 
-// part1: 6087
-// part2: 2493
+// part1: 11220
+// part2:
+// ###..####.###...##....##.####.#....#..#.
+// #..#....#.#..#.#..#....#.#....#....#.#..
+// ###....#..#..#.#..#....#.###..#....##...
+// #..#..#...###..####....#.#....#....#.#..
+// #..#.#....#....#..#.#..#.#....#....#.#..
+// ###..####.#....#..#..##..####.####.#..#.
