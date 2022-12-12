c409c677eb425e4d0be08f1351234b18850fe1c4
Jonas Keating <jonask@microsoft.com>
2022-12-12 11:13:19 -0800

    Day 12

--- 2022/src/d12/mod.rs
+++ 2022/src/d12/mod.rs
@@ -1,93 +1,120 @@
-use std::{fs};
+use std::{fs, collections::VecDeque, time::Instant};
 
 use ::function_name::named;
 //use itertools::Itertools;
 //use lazy_static::lazy_static;
 
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+struct Point {
+    pub r: i32,
+    pub c: i32
+}
+
+fn valid_vec_index<T>(v: &Vec<T>, index: i32) -> bool {
+    0 <= index && index < v.len() as i32
+}
+
+fn alloc_2d_vec<T: Clone>(height: usize, width: usize, val: T) -> Vec<Vec<T>> {
+    let mut ret: Vec<Vec<T>> = Vec::new();
+    ret.resize(height, Vec::new());
+    for i in 0..height {
+        ret[i].resize(width, val.clone());
+    }
+    ret
+}
+
+fn process_neighbor(
+    grid: &Vec<Vec<char>>,
+    best_distances: &mut Vec<Vec<i32>>,
+    loc: Point,
+    from_height: char,
+    mut distance: i32,
+    could_be_start: Option<fn(char) -> bool>,
+    to_process: &mut VecDeque<Point>
+){
+    if !valid_vec_index(&grid, loc.r) || !valid_vec_index(&grid[0], loc.c) {
+        return;
+    }
+    let to_height = grid[loc.r as usize][loc.c as usize];
+    if to_height as i32 - from_height as i32 > 1 {
+        return;
+    }
+    if could_be_start.is_some() && could_be_start.unwrap()(to_height) {
+        distance = 0;
+    }
+    if distance >= best_distances[loc.r as usize][loc.c as usize] {
+        return;
+    }
+    best_distances[loc.r as usize][loc.c as usize] = distance;
+    to_process.push_back(loc);
+}
+
+fn find_shortest_path(valid_start_loc_test: Option<fn(char) -> bool>) -> i32 {
+    // Read grid
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    let file_contents = fs::read_to_string("src\\d12\\data.txt").expect("Error loading file");
+    for line in file_contents.split("\r\n") {
+        grid.push(line.chars().map(|c| c).collect());
+    }
+    
+    // Find start and end points
+    let mut start = Point { r: 0, c:0 };
+    let mut end = Point { r: 0, c:0 };
+    let (height, width) = (grid.len(), grid[0].len());
+
+    for (row_index, row) in grid.iter().enumerate() {
+        for (col_index, val) in row.iter().enumerate() {
+            match val {
+                'S' => { start = Point { r: row_index as i32, c: col_index as i32 }; }
+                'E' => { end = Point { r: row_index as i32, c: col_index as i32  }; }
+                _ => {}
+            }    
+        }    
+    }
+    // Set height values in start/end
+    grid[start.r as usize][start.c as usize] = 'a';
+    grid[end.r as usize][end.c as usize] = 'z';
+
+    // Grid for shortest distance to each point
+    let mut best_distances = alloc_2d_vec(height, width, (height * width) as i32);
+    best_distances[start.r as usize][start.c as usize] = 0;
+
+    // Start with the start point
+    let mut to_process: VecDeque<Point> = VecDeque::new();
+    to_process.push_back(start);
+    
+    // Do the work
+    while !to_process.is_empty() {
+        let p = to_process.pop_front().unwrap();
+        let height = grid[p.r as usize][p.c as usize];
+        let dist = best_distances[p.r as usize][p.c as usize];
+        process_neighbor(&grid, &mut best_distances, Point { r: p.r - 1, c: p.c }, height, dist + 1, valid_start_loc_test, &mut to_process);
+        process_neighbor(&grid, &mut best_distances, Point { r: p.r + 1, c: p.c }, height, dist + 1, valid_start_loc_test, &mut to_process);
+        process_neighbor(&grid, &mut best_distances, Point { r: p.r, c: p.c - 1 }, height, dist + 1, valid_start_loc_test, &mut to_process);
+        process_neighbor(&grid, &mut best_distances, Point { r: p.r, c: p.c + 1 }, height, dist + 1, valid_start_loc_test, &mut to_process);
+    }
+
+    best_distances[end.r as usize][end.c as usize]
+}
+
 #[named]
 fn part1() {
-    let file_contents = fs::read_to_string("src\\d12\\data.txt").expect("Error loading file");
-    let mut file_lines = file_contents.split("\r\n");
+    let now = Instant::now();
 
-    let mut iteration_count = 0;
-    let mut register = 1;
-    let mut pending_write: Option<i32> = None;
-    let mut total_strength = 0;
-
-    loop {
-        iteration_count += 1;
-
-        // Use Register here
-        //println!("{}: {} ({:?})", iteration_count, register, pending_write);
-        if (iteration_count - 20) % 40 == 0 {
-            total_strength += iteration_count * register;
-        }
-
-        if pending_write.is_some() {
-            register = pending_write.unwrap();
-            pending_write = None;
-            continue;
-        }
-
-        let next_line = file_lines.next();
-        if next_line.is_none() {
-            break;
-        }
-        
-        let instruction = next_line.unwrap().split(' ').collect::<Vec<&str>>();
-        assert!((instruction.len() == 1 && instruction[0] == "noop") || (instruction.len() == 2 && instruction[0] == "addx"));
-        match instruction[0] {
-            "noop" => { }
-            "addx" => {
-                pending_write = Some(register + instruction[1].parse::<i32>().unwrap());
-            }
-            _ => { panic!() }
-        }
-    }
-    println!("{}: {}", function_name!(), total_strength);
+    // There is no other potential start location beyond the initial 'S'
+    let valid_start_loc_test: Option<fn(char) -> bool> = None;
+    let shortest_path = find_shortest_path(valid_start_loc_test);
+    println!("{}: {} ({} ms)", function_name!(), shortest_path, now.elapsed().as_millis());
 }
 
 #[named]
 fn part2() {
-    let file_contents = fs::read_to_string("src\\d12\\data.txt").expect("Error loading file");
-    let mut file_lines = file_contents.split("\r\n");
+    let now = Instant::now();
 
-    let mut iteration_count = 0;
-    let mut register = 1;
-    let mut pending_write: Option<i32> = None;
-    
-    println!("{}:", function_name!());
-    loop {
-        iteration_count += 1;
-        let crt_index = (iteration_count - 1) % 40;
-
-        print!("{}", if num::abs(crt_index - register) < 2 { '#' } else { '.'});
-
-        if iteration_count % 40 == 0 {
-            println!("");
-        }
-
-        if pending_write.is_some() {
-            register = pending_write.unwrap();
-            pending_write = None;
-            continue;
-        }
-
-        let next_line = file_lines.next();
-        if next_line.is_none() {
-            break;
-        }
-
-        let instruction = next_line.unwrap().split(' ').collect::<Vec<&str>>();
-        assert!((instruction.len() == 1 && instruction[0] == "noop") || (instruction.len() == 2 && instruction[0] == "addx"));
-        match instruction[0] {
-            "noop" => { }
-            "addx" => {
-                pending_write = Some(register + instruction[1].parse::<i32>().unwrap());
-            }
-            _ => { panic!() }
-        }
-    }
+    // Any 'a' in the grid can be a valid start location
+    let valid_start_loc_test: Option<fn(char) -> bool> = Some(|c| c == 'a');
+    let shortest_path = find_shortest_path(valid_start_loc_test);
+    println!("{}: {} ({} ms)", function_name!(), shortest_path, now.elapsed().as_millis());
 }
 
 pub fn run() {
@@ -95,11 +122,5 @@ pub fn run() {
     part2();
 }
 
-// part1: 11220
-// part2:
-// ###..####.###...##....##.####.#....#..#.
-// #..#....#.#..#.#..#....#.#....#....#.#..
-// ###....#..#..#.#..#....#.###..#....##...
-// #..#..#...###..####....#.#....#....#.#..
-// #..#.#....#....#..#.#..#.#....#....#.#..
-// ###..####.#....#..#..##..####.####.#..#.
+// part1: 350
+// part2: 349
