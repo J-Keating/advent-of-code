30bcfcf6b109e3a1b7882ccf52462548af05d4d0
Jonas Keating <jonask@microsoft.com>
2023-01-16 12:24:29 -0800

    Days 3 and 4

---
+++ 2015/src/d3/mod.rs
@@ -0,0 +1,60 @@
+use std::collections::HashSet;
+use std::fs;
+use std::str::FromStr;
+use itertools::Itertools;
+
+use ::function_name::named;
+
+fn move_in_direction(curr: (i32, i32), c: char) -> (i32, i32) {
+    match c {
+        '^' => (curr.0, curr.1 + 1),
+        'v' => (curr.0, curr.1 - 1),
+        '>' => (curr.0 + 1, curr.1),
+        '<' => (curr.0 - 1, curr.1),
+        _ => { panic!("Unknown direction") }
+    }
+}
+
+#[named]
+fn part1() {
+    let data = fs::read_to_string("src\\d3\\data.txt").unwrap();
+    for line in data.lines() {
+        let mut visited: HashSet<(i32, i32)> = HashSet::new();
+        let mut curr = (0, 0);
+        visited.insert(curr);
+        for c in line.chars().step_by(1) {
+            curr = move_in_direction(curr, c);
+            visited.insert(curr);
+        }
+        println!("{}: {}", function_name!(), visited.len());
+    }
+}
+
+#[named]
+fn part2() {
+    let data = fs::read_to_string("src\\d3\\data.txt").unwrap();
+    for line in data.lines() {
+        let mut visited: HashSet<(i32, i32)> = HashSet::new();
+        let mut curr = (0, 0);
+        visited.insert(curr);
+        for c in line.chars().step_by(2) {
+            curr = move_in_direction(curr, c);
+            visited.insert(curr);
+        }
+        curr = (0, 0);
+        for c in line.chars().skip(1).step_by(2) {
+            curr = move_in_direction(curr, c);
+            visited.insert(curr);
+        }
+        println!("{}: {}", function_name!(), visited.len());
+    }
+}
+
+pub fn run() {
+    part1();
+    part2();
+}
+
+// part1: 2572
+// part2: 3783758
+ 
---
+++ 2015/src/d4/mod.rs
@@ -0,0 +1,61 @@
+use std::{str::FromStr, time::Instant};
+use itertools::Itertools;
+use md5::{Md5, Digest};
+
+use ::function_name::named;
+
+type HashResultType = crypto_common::generic_array::GenericArray<u8, crypto_common::typenum::UInt<crypto_common::typenum::UInt<crypto_common::typenum::UInt<crypto_common::typenum::UInt<crypto_common::typenum::UInt<crypto_common::typenum::UTerm, crypto_common::typenum::B1>, crypto_common::typenum::B0>, crypto_common::typenum::B0>, crypto_common::typenum::B0>, crypto_common::typenum::B0>>;
+
+fn find_first_key(prefix: &str, filter_function: fn(&HashResultType)->bool) -> u32 {
+    let mut hasher = Md5::new();
+    for i in 0.. {
+        hasher.update(format!("{}{}", prefix, i).as_bytes());
+        if filter_function(&hasher.finalize_reset()) {
+            return i;
+        }
+    }
+    panic!()
+}
+
+fn find_first_key_2(prefix: &str, filter_function: fn(&HashResultType)->bool) -> u32 {
+    let mut hasher = Md5::new();
+    itertools::iterate(0, |i| i + 1)
+        .filter(|i| {
+            hasher.update(format!("{}{}", prefix, i).as_bytes());
+            filter_function(&hasher.finalize_reset())
+        })
+        .take(1)
+        .last()
+        .unwrap()
+}
+
+#[named]
+fn part1() {
+    // Test 
+    // println!("{} (test): {}", function_name!(), find_first_key("abcdef"));
+    // println!("{} (test): {}", function_name!(), find_first_key("pqrstuv"));
+    let mut now = Instant::now();
+    println!("{}: {} ({} sec)", function_name!(), find_first_key("bgvyzdsv", |x| x[0] == 0 && x[1] == 0 && x[2] < 16), now.elapsed().as_millis() as f32 / 1000.0);
+    now = Instant::now();
+    println!("{}: {} ({} sec)", function_name!(), find_first_key_2("bgvyzdsv", |x| x[0] == 0 && x[1] == 0 && x[2] < 16), now.elapsed().as_millis() as f32 / 1000.0);
+}
+
+#[named]
+fn part2() {
+    let mut now = Instant::now();
+    println!("{}: {} ({} sec)", function_name!(), find_first_key("bgvyzdsv", |x| x[0] == 0 && x[1] == 0 && x[2] == 0), now.elapsed().as_millis() as f32 / 1000.0);
+    now = Instant::now();
+    println!("{}: {} ({} sec)", function_name!(), find_first_key_2("bgvyzdsv", |x| x[0] == 0 && x[1] == 0 && x[2] == 0), now.elapsed().as_millis() as f32 / 1000.0);
+}
+
+pub fn run() {
+    part1();
+    part2();
+}
+
+// Day4:
+// part1: 254575 (1.185 sec)
+// part1: 254575 (1.207 sec)
+// part2: 1038736 (4.832 sec)
+// part2: 1038736 (4.906 sec)
+ 
