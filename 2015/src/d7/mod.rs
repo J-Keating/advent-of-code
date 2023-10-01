95cfd945e80742398ea0a3427282c512f43a91be
Jonas Keating <jonask@microsoft.com>
2023-10-01 12:18:30 -0700

    Day 7 complete

--- 2015/src/d7/mod.rs
+++ 2015/src/d7/mod.rs
@@ -1,7 +1,7 @@
 use ::function_name::named;
+use itertools::Itertools;
 use regex::Regex;
-use std::{cmp, fs, str::FromStr, collections::HashMap};
-use utilities::alloc_2d_vec;
+use std::{collections::HashMap, fs};
 
 const DAY: i32 = 7;
 
@@ -20,34 +20,28 @@ struct Instruction {
     operation: Operation,
     arg1: String,
     arg2: Option<String>,
-    dest: String,
 }
 
-fn load_data(filename: &str) -> Vec<Instruction> {
+fn load_data(filename: &str) -> HashMap<String, Instruction> {
     let data = fs::read_to_string(filename).unwrap();
-    let mut instructions: Vec<Instruction> = Vec::new();
+    let mut connections: HashMap<String, Instruction> = HashMap::new();
     let re = Regex::new(r"(.*)\s+->\s+(.*)").unwrap();
     for line in data.lines() {
         for cap in re.captures_iter(line) {
-            println!("Matched: {} | {}", &cap[1], &cap[2]);
             let dest: String = cap[2].to_string();
             let split_operation: Vec<&str> = cap[1].split_whitespace().collect();
             let instruction = match split_operation.len() {
-                1 => {
-                    Instruction {
-                        operation: Operation::SET,
-                        arg1: split_operation[0].to_string(),
-                        arg2: None,
-                        dest: dest,
-                    }
-                }
+                1 => Instruction {
+                    operation: Operation::SET,
+                    arg1: split_operation[0].to_string(),
+                    arg2: None,
+                },
                 2 => {
                     assert!(split_operation[0] == "NOT");
                     Instruction {
                         operation: Operation::NOT,
                         arg1: split_operation[1].to_string(),
                         arg2: None,
-                        dest: dest,
                     }
                 }
                 3 => {
@@ -62,71 +56,78 @@ fn load_data(filename: &str) -> Vec<Instruction> {
                         operation: operation,
                         arg1: split_operation[0].to_string(),
                         arg2: Some(split_operation[2].to_string()),
-                        dest: dest,
                     }
                 }
                 _ => panic!("Invalid line: {:?}", split_operation),
             };
-            instructions.push(instruction);
+            connections.insert(dest, instruction);
         }
     }
-    instructions
+    connections
 }
 
-fn run_instructions(instructions: &Vec<Instruction>) -> HashMap<String, u16> {
-    let mut map: HashMap<String, u16> = HashMap::new();
-    for instruction in instructions {
-        let arg1 = match instruction.arg1.parse::<u16>() {
-            Ok(n) => n,
-            Err(_) => match map.get(&instruction.arg1) {
-                Some(n) => *n,
-                None => panic!("Unknown arg1: {}", instruction.arg1),
-            },
-        };
-        let arg2 = match instruction.arg2 {
-            Some(ref arg2) => match arg2.parse::<u16>() {
-                Ok(n) => n,
-                Err(_) => match map.get(arg2) {
-                    Some(n) => *n,
-                    None => panic!("Unknown arg2: {}", arg2),
-                },
-            },
-            None => 0,
-        };
-        let result = match instruction.operation {
-            Operation::SET => arg1,
-            Operation::NOT => !arg1,
-            Operation::AND => arg1 & arg2,
-            Operation::LSHIFT => arg1 << arg2,
-            Operation::RSHIFT => arg1 >> arg2,
-            Operation::OR => arg1 | arg2,
-        };
-        map.insert(instruction.dest.clone(), result);
-        //println!("{:?}", map);
+fn evaluate_signal(connections: &HashMap<String, Instruction>, wire: &String, signal_cache: &mut HashMap<String, u16>) -> u16 {
+    if signal_cache.contains_key(wire) {
+        return *signal_cache.get(wire).unwrap();
     }
-    map
+    let instruction: &Instruction = connections.get(wire).unwrap();
+    let arg1 = match instruction.arg1.parse::<u16>() {
+        Ok(n) => n,
+        Err(_) => evaluate_signal(connections, &instruction.arg1, signal_cache),
+    };
+    let arg2 = match instruction.arg2 {
+        Some(ref arg2) => match arg2.parse::<u16>() {
+            Ok(n) => n,
+            Err(_) => evaluate_signal(connections, arg2, signal_cache),
+        },
+        None => 0,
+    };
+    let result = match instruction.operation {
+        Operation::SET => arg1,
+        Operation::NOT => !arg1,
+        Operation::AND => arg1 & arg2,
+        Operation::LSHIFT => arg1 << arg2,
+        Operation::RSHIFT => arg1 >> arg2,
+        Operation::OR => arg1 | arg2,
+    };
+    signal_cache.insert(wire.to_string(), result);
+    result
 }
+
+// #[named]
+// fn part1_test() {
+//     let connections = load_data(&format!("src\\d{}\\data_test.txt", DAY));
+//     let mut signal_cache: HashMap<String, u16> = HashMap::new();
+//     connections.keys().sorted().for_each(|k| {
+//         let res = evaluate_signal(&connections, k, &mut signal_cache);
+//         println!("{}: {} = {}", function_name!(), k, res);
+//     });
+// }
 
 #[named]
 fn part1() {
-    let instructions = load_data(&format!("src\\d{}\\data.txt", DAY));
-    let map = run_instructions(&instructions);
-        println!("{:?}", map);
-        println!("{}: a = {:?}", function_name!(), map["a"]);
+    let connections = load_data(&format!("src\\d{}\\data.txt", DAY));
+    let mut signal_cache: HashMap<String, u16> = HashMap::new();
+    let res = evaluate_signal(&connections, &"a".to_string(), &mut signal_cache);
+    println!("{}: a = {:?}", function_name!(), res);
 }
 
 #[named]
-// fn part2() {
-//     let instructions = load_data(&format!("src\\d{}\\data_test.txt", DAY));
-//     let map = run_instructions(&instructions);
-//     println!("{}: {:?}", function_name!(), map);
-// }
+fn part2() {
+    // part1: a = 3176
+    let connections = load_data(&format!("src\\d{}\\data.txt", DAY));
+    let mut signal_cache: HashMap<String, u16> = HashMap::new();
+    signal_cache.insert("b".to_string(), 3176);
+    let res = evaluate_signal(&connections, &"a".to_string(), &mut signal_cache);
+    println!("{}: a = {:?}", function_name!(), res);
+}
 
 pub fn run() {
     println!("Day {}:", DAY);
+    //part1_test();
     part1();
-    //part2();
+    part2();
 }
 
-// part1: 377891
-// part2: 14110788
+// part1: a = 3176
+// part2: a = 14710
