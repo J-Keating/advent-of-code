6f161433b5ac5e587cfee8c6e81b44d4cedf0dea
Jonas Keating <jonask@microsoft.com>
2023-10-01 11:41:35 -0700

    Saving work - bad assumptions about synchronous Execution.

--- 2015/src/d7/mod.rs
+++ 2015/src/d7/mod.rs
@@ -1,98 +1,132 @@
-use std::{fs, str::FromStr, cmp};
 use ::function_name::named;
 use regex::Regex;
-use utilities::{alloc_2d_vec};
+use std::{cmp, fs, str::FromStr, collections::HashMap};
+use utilities::alloc_2d_vec;
 
 const DAY: i32 = 7;
 
-#[test]
-fn test_regex() {
-    // turn on 887,9 through 959,629
-    let re = Regex::new(r"(turn on|turn off|toggle)\s+(\d+),(\d+) through (\d+),(\d+).*").unwrap();
-    let text = "turn on 454,398 through 844,448";
-    for cap in re.captures_iter(text) {
-        println!("Matched: {} {} {} {} {}", &cap[1], &cap[2], &cap[3], &cap[4], &cap[5]);
-    }
+#[derive(Debug, Clone)]
+enum Operation {
+    SET,
+    NOT,
+    AND,
+    LSHIFT,
+    RSHIFT,
+    OR,
 }
 
-#[derive(Debug, Copy, Clone)]
-enum Action {
-    TurnOn,
-    TurnOff,
-    Toggle,
-}
-
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 struct Instruction {
-    action: Action,
-    start: (usize, usize),
-    end: (usize, usize),
+    operation: Operation,
+    arg1: String,
+    arg2: Option<String>,
+    dest: String,
 }
 
 fn load_data(filename: &str) -> Vec<Instruction> {
     let data = fs::read_to_string(filename).unwrap();
-    let mut instructions = Vec::new();
-    let re = Regex::new(r"(turn on|turn off|toggle)\s+(\d+),(\d+) through (\d+),(\d+).*").unwrap();
+    let mut instructions: Vec<Instruction> = Vec::new();
+    let re = Regex::new(r"(.*)\s+->\s+(.*)").unwrap();
     for line in data.lines() {
         for cap in re.captures_iter(line) {
-            let action = match &cap[1] {
-                "turn on" => Action::TurnOn,
-                "turn off" => Action::TurnOff,
-                "toggle" => Action::Toggle,
-                _ => panic!("Unknown action: {}", &cap[1]),
+            println!("Matched: {} | {}", &cap[1], &cap[2]);
+            let dest: String = cap[2].to_string();
+            let split_operation: Vec<&str> = cap[1].split_whitespace().collect();
+            let instruction = match split_operation.len() {
+                1 => {
+                    Instruction {
+                        operation: Operation::SET,
+                        arg1: split_operation[0].to_string(),
+                        arg2: None,
+                        dest: dest,
+                    }
+                }
+                2 => {
+                    assert!(split_operation[0] == "NOT");
+                    Instruction {
+                        operation: Operation::NOT,
+                        arg1: split_operation[1].to_string(),
+                        arg2: None,
+                        dest: dest,
+                    }
+                }
+                3 => {
+                    let operation = match split_operation[1] {
+                        "AND" => Operation::AND,
+                        "LSHIFT" => Operation::LSHIFT,
+                        "RSHIFT" => Operation::RSHIFT,
+                        "OR" => Operation::OR,
+                        _ => panic!("Unknown operation: {}", split_operation[1]),
+                    };
+                    Instruction {
+                        operation: operation,
+                        arg1: split_operation[0].to_string(),
+                        arg2: Some(split_operation[2].to_string()),
+                        dest: dest,
+                    }
+                }
+                _ => panic!("Invalid line: {:?}", split_operation),
             };
-            let start = (usize::from_str(&cap[2]).unwrap(), usize::from_str(&cap[3]).unwrap());
-            let end = (usize::from_str(&cap[4]).unwrap(), usize::from_str(&cap[5]).unwrap());
-            instructions.push(Instruction { action, start, end });
+            instructions.push(instruction);
         }
     }
     instructions
 }
 
-#[named]
-fn part1() {
-    let instructions = load_data(&format!("src\\d{}\\data.txt", DAY));
-    let mut board = alloc_2d_vec(1000, 1000, false);
+fn run_instructions(instructions: &Vec<Instruction>) -> HashMap<String, u16> {
+    let mut map: HashMap<String, u16> = HashMap::new();
     for instruction in instructions {
-        for x in instruction.start.0..=instruction.end.0 {
-            for y in instruction.start.1..=instruction.end.1 {
-                match instruction.action {
-                    Action::TurnOn => board[x][y] = true,
-                    Action::TurnOff => board[x][y] = false,
-                    Action::Toggle => board[x][y] = !board[x][y],
-                }
-            }
-        }
+        let arg1 = match instruction.arg1.parse::<u16>() {
+            Ok(n) => n,
+            Err(_) => match map.get(&instruction.arg1) {
+                Some(n) => *n,
+                None => panic!("Unknown arg1: {}", instruction.arg1),
+            },
+        };
+        let arg2 = match instruction.arg2 {
+            Some(ref arg2) => match arg2.parse::<u16>() {
+                Ok(n) => n,
+                Err(_) => match map.get(arg2) {
+                    Some(n) => *n,
+                    None => panic!("Unknown arg2: {}", arg2),
+                },
+            },
+            None => 0,
+        };
+        let result = match instruction.operation {
+            Operation::SET => arg1,
+            Operation::NOT => !arg1,
+            Operation::AND => arg1 & arg2,
+            Operation::LSHIFT => arg1 << arg2,
+            Operation::RSHIFT => arg1 >> arg2,
+            Operation::OR => arg1 | arg2,
+        };
+        map.insert(instruction.dest.clone(), result);
+        //println!("{:?}", map);
     }
-    let count = board.iter().map(|row| row.iter().filter(|&b| *b).count()).sum::<usize>();
-    println!("{}: {}", function_name!(), count);
+    map
 }
 
 #[named]
-fn part2() {
+fn part1() {
     let instructions = load_data(&format!("src\\d{}\\data.txt", DAY));
-    let mut board = alloc_2d_vec(1000, 1000, 0);
-    for instruction in instructions {
-        for x in instruction.start.0..=instruction.end.0 {
-            for y in instruction.start.1..=instruction.end.1 {
-                match instruction.action {
-                    Action::TurnOn => board[x][y] += 1,
-                    Action::TurnOff => board[x][y] = cmp::max(0, board[x][y] - 1),
-                    Action::Toggle => board[x][y] += 2,
-                }
-            }
-        }
-    }
-    let total_brightness = board.iter().map(|row| row.iter().sum::<i32>()).sum::<i32>();
-    println!("{}: {}", function_name!(), total_brightness);
+    let map = run_instructions(&instructions);
+        println!("{:?}", map);
+        println!("{}: a = {:?}", function_name!(), map["a"]);
 }
+
+#[named]
+// fn part2() {
+//     let instructions = load_data(&format!("src\\d{}\\data_test.txt", DAY));
+//     let map = run_instructions(&instructions);
+//     println!("{}: {:?}", function_name!(), map);
+// }
 
 pub fn run() {
     println!("Day {}:", DAY);
     part1();
-    part2();
+    //part2();
 }
 
 // part1: 377891
 // part2: 14110788
- 
