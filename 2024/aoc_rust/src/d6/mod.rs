755a792c5a2cdc3aa5e8db78e830d0f448817140
Jonas Keating <jonask@microsoft.com>
2024-12-12 00:34:15 +0100

    D6 P2

--- 2024/aoc_rust/src/d6/mod.rs
+++ 2024/aoc_rust/src/d6/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet};
 use std::{fs};
 
 use ::function_name::named;
@@ -10,7 +10,8 @@ enum Direction {
     Up,
     Down,
     Left,
-    Right
+    Right,
+    Empty
 }
 
 fn turn_right(direction: Direction) -> Direction {
@@ -18,7 +19,8 @@ fn turn_right(direction: Direction) -> Direction {
         Direction::Up => Direction::Right,
         Direction::Down => Direction::Left,
         Direction::Left => Direction::Up,
-        Direction::Right => Direction::Down
+        Direction::Right => Direction::Down,
+        _ => panic!("Invalid direction")
     }
 }
 
@@ -27,7 +29,8 @@ fn step(direction: Direction) -> PointRC {
         Direction::Up => PointRC { r: -1, c: 0 },
         Direction::Down => PointRC { r: 1, c: 0 },
         Direction::Left => PointRC { r: 0, c: -1 },
-        Direction::Right => PointRC { r: 0, c: 1 }
+        Direction::Right => PointRC { r: 0, c: 1 },
+        _ => panic!("Invalid direction"),
     }
 }
 
@@ -51,6 +54,25 @@ impl Board {
         let mut buff = self.data.clone();
         for point in path {
             buff[point.r as usize][point.c as usize] = 'x';
+        }
+        for line in buff {
+            println!("{}", line.iter().collect::<String>());
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn print2(&self, visited: &Vec<Vec<Direction>>) {
+        let mut buff = self.data.clone();
+        for r in 0..self.height {
+            for c in 0..self.width {
+                buff[r][c] = match visited[r][c] {
+                    Direction::Up => '^',
+                    Direction::Down => 'v',
+                    Direction::Left => '<',
+                    Direction::Right => '>',
+                    Direction::Empty => ' ',
+                };
+            }
         }
         for line in buff {
             println!("{}", line.iter().collect::<String>());
@@ -87,36 +109,80 @@ fn load_data(path: &str) -> Board {
     board
 }
 
-#[named]
-fn part1() {
-    let mut board = load_data("src\\d6\\data.txt");
-    let mut loc = board.find('^').unwrap();
-    board.data[loc.r as usize][loc.c as usize] = '.';
+fn run_simulation(board: &Board, start_loc: PointRC) -> HashSet<PointRC> {
+    let mut loc = start_loc;
     let mut dir = Direction::Up;
-    let mut visited: HashSet<PointRC> = HashSet::new();
+    let mut visited = HashSet::new();
+    visited.insert(loc);
     loop {
-        visited.insert(loc);
         let next_loc = loc.add(&step(dir));
         if !board.in_bounds(next_loc) {
             break;
         }
-        if board.data[next_loc.r as usize][next_loc.c as usize] != '.' {
+        if board.data[next_loc.r as usize][next_loc.c as usize] == '#' {
+            dir = turn_right(dir);
+        }
+        else {
+            loc = next_loc;
+            visited.insert(loc);
+        }
+    }
+    visited
+}
+#[named]
+fn part1() {
+    let board = load_data("src\\d6\\data.txt");
+    let loc = board.find('^').unwrap();
+    let visited = run_simulation(&board, loc);
+    println!("{}: {}", function_name!(), visited.len());
+}
+
+fn does_loop(board: &Board, start_loc: PointRC, start_dir: Direction) -> bool {
+    let mut loc = start_loc;
+    let mut dir = start_dir;
+    let mut visited = alloc_2d_vec::<Direction>(board.height, board.width, Direction::Empty);
+
+    let mut count = 0;
+    loop {
+        visited[loc.r as usize][loc.c as usize] = dir;
+        let next_loc = loc.add(&step(dir));
+        if !board.in_bounds(next_loc) {
+            return false;
+        }
+        if board.data[next_loc.r as usize][next_loc.c as usize] == '#' {
             dir = turn_right(dir);
         }
         else {
             loc = next_loc;
         }
-        //board.print(&visited);
+        if visited[loc.r as usize][loc.c as usize] == dir {
+            return true;
+        }
+        count += 1;
+        if count > 1 * 1000 * 1000 {
+            //board.print2(&visited);
+            return true;
+        }
     }
-    board.print(&visited);
-    println!("{}: {}", function_name!(), visited.len());
 }
 
 #[named]
 fn part2() {
-    let board = load_data("src\\d5\\data_test.txt");
-    println!("{}: {}", function_name!(), board.height);
+    let mut board = load_data("src\\d6\\data.txt");
+    let start_loc = board.find('^').unwrap();
+    let mut visited = run_simulation(&board, start_loc);
+    visited.remove(&start_loc);
 
+    let mut block_locs: HashSet<PointRC> = HashSet::new();
+    for test_loc in visited.iter() {
+        let old = board.data[test_loc.r as usize][test_loc.c as usize];
+        board.data[test_loc.r as usize][test_loc.c as usize] = '#';
+        if does_loop(&board, start_loc, Direction::Up) {
+            block_locs.insert(*test_loc);
+        }
+        board.data[test_loc.r as usize][test_loc.c as usize] = old;
+    }
+    println!("{}: {}", function_name!(), block_locs.len());
 }
 
 pub fn run() {
@@ -124,5 +190,5 @@ pub fn run() {
     part2();
 }
 
-// part1: 5509
-// part2: 4407
+// part1: 4752
+// part2: 1719
