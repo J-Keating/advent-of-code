746a81686e38cbeca4d8831ad703a7226edfa9d9
Jonas Keating <jonask@microsoft.com>
2024-12-12 17:44:24 +0100

    D7 P2

--- 2024/aoc_rust/src/d7/mod.rs
+++ 2024/aoc_rust/src/d7/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::fs;
 
 use ::function_name::named;
 
@@ -9,37 +9,57 @@ fn load_data(path: &str) -> Vec<(i64, Vec<i64>)> {
     for line in file_lines {
         let mut parts = line.split(": ");
         let depth = parts.next().unwrap().parse().unwrap();
-        let range = parts.next().unwrap().split(" ").map(|s| s.parse().unwrap()).collect();
+        let range = parts
+            .next()
+            .unwrap()
+            .split(" ")
+            .map(|s| s.parse().unwrap())
+            .collect();
         ret.push((depth, range));
     }
     ret
 }
 
-fn run_math(target: i64, current: i64, index: i32, nums: &Vec<i64>) -> i32{
+fn run_math_1(target: i64, current: i64, index: i32, nums: &Vec<i64>) -> bool {
     if index == nums.len() as i32 {
-        return if current == target { 1 } else { 0 };
+        return current == target;
     }
-    run_math(target, current + nums[index as usize], index + 1, nums) + run_math(target, current * nums[index as usize], index + 1, nums)
+    run_math_1(target, current + nums[index as usize], index + 1, nums)
+        || run_math_1(target, current * nums[index as usize], index + 1, nums)
+}
+
+fn run_math_2(target: i64, current: i64, index: i32, nums: &Vec<i64>) -> bool {
+    if index == nums.len() as i32 {
+        return current == target;
+    }
+    run_math_2(target, current + nums[index as usize], index + 1, nums)
+        || run_math_2(target, current * nums[index as usize], index + 1, nums)
+        || {
+            let new_current = (current.to_string() + &nums[index as usize].to_string()).parse().unwrap();
+            run_math_2(target, new_current, index + 1, nums)
+        }
 }
 
 #[named]
 fn part1() {
     let data = load_data("src\\d7\\data.txt");
-    let mut sum = 0;
-    for (target, nums) in data {
-        let result = run_math(target, nums[0], 1, &nums);
-        if result > 0 {
-            sum += target;
-        }
-        //println!("{:?}: {}", nums, result);
-    }
+    let sum = data
+        .iter()
+        .filter(|(target, nums)| run_math_1(*target, nums[0], 1, nums))
+        .map(|(target, _)| target)
+        .sum::<i64>();
     println!("{}: {}", function_name!(), sum);
 }
 
 #[named]
 fn part2() {
     let data = load_data("src\\d7\\data.txt");
-    println!("{}: {}", function_name!(), data.len());
+    let sum = data
+        .iter()
+        .filter(|(target, nums)| run_math_2(*target, nums[0], 1, nums))
+        .map(|(target, _)| target)
+        .sum::<i64>();
+    println!("{}: {}", function_name!(), sum);
 }
 
 pub fn run() {
@@ -48,4 +68,4 @@ pub fn run() {
 }
 
 // part1: 945512582195
-// part2: 850
+// part2: 271691107779347
