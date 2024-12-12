9ec7c63469069c71e4e5f8440e79fe8b4f13f370
Jonas Keating <jonask@microsoft.com>
2024-12-13 00:29:29 +0100

    placeholders for d9-13

---
+++ 2024/aoc_rust/src/d10/mod.rs
@@ -0,0 +1,27 @@
+use itertools::Itertools;
+use std::fs;
+use std::collections::{HashMap, HashSet};
+use utilities::{Board, PointRC};
+use ::function_name::named;
+
+const DAY: &str = "d10";
+
+#[named]
+fn part1() {
+    let file_contents_as_string = fs::read_to_string(&("src\\".to_string() + DAY + "\\data_test.txt")).expect("Error loading file");
+    println!("{}: {}", function_name!(), file_contents_as_string);
+}
+
+#[named]
+fn part2() {
+    let file_contents_as_string = fs::read_to_string(&("src\\".to_string() + DAY + "\\data_test.txt")).expect("Error loading file");
+    println!("{}: {}", function_name!(), file_contents_as_string);
+}
+
+pub fn run() {
+    part1();
+    part2();
+}
+
+// part1: 327
+// part2: 1233
