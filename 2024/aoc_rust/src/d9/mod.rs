226f7da7f04e2952d05b91fc00e828d800f21534
Jonas Keating <jonask@microsoft.com>
2024-12-31 17:07:37 -0800

    D9 P2

--- 2024/aoc_rust/src/d9/mod.rs
+++ 2024/aoc_rust/src/d9/mod.rs
@@ -51,10 +51,8 @@ fn compute_checksum_old(buffer: &[Option<i32>]) -> i64 {
     checksum
 }
 
-#[named]
 fn compute_checksum(buffer: &[Option<i32>]) -> i64 {
     let checksum = buffer.iter().enumerate().filter(|(_, val)| { val.is_some() }).map(|(i, val)| { i as i64 * val.unwrap() as i64 }).sum();
-    println!("{}: {}", function_name!(), checksum);
     checksum
 }
 
@@ -95,6 +93,7 @@ fn expand_disk_2(map: &str) -> Vec<(Option<i32>, usize)> {
     buffer
 }
 
+#[allow(dead_code)]
 fn print_disk_2(buffer: &Vec<(Option<i32>, usize)>) {
     buffer.iter().for_each(|(val, count)| {
         for _ in 0..*count {
@@ -104,6 +103,7 @@ fn print_disk_2(buffer: &Vec<(Option<i32>, usize)>) {
                 print!(".");
             }
         }
+        print!("|");
     });
     println!();
 }
@@ -111,30 +111,59 @@ fn print_disk_2(buffer: &Vec<(Option<i32>, usize)>) {
 fn compute_checksum_2(buffer: &Vec<(Option<i32>, usize)>) -> i64 {
     let mut index: usize = 0;
     buffer.iter().map(|(val, count)| {
-        if val.is_some() {
-            let actual_val = val.unwrap() as i64;
-            let run_sum = (0..*count).into_iter().map(|i| {
-                (index + i) as i64 * actual_val
-            }).sum();
-            index += *count;
-            run_sum
-        } else {
-            index += *count;
-            0
-        }
+        let run_sum = match val {
+            Some(val) => (0..*count).into_iter().map(|i| {
+                (index + i) as i64 * *val as i64
+            }).sum(),
+            None => 0,
+        };
+        index += *count;
+        run_sum
     }).sum()
+}
+
+fn try_move_block(buffer: &mut Vec<(Option<i32>, usize)>, disk_id: i32) {
+    let index = buffer.iter().position(|(val, _)| {
+        val.is_some() && val.unwrap() == disk_id
+    }).unwrap();
+    let (block_val, block_size_needed) = buffer[index];
+    let dest = buffer.iter().position(|(val, size)| {
+        val.is_none() && *size >= block_size_needed
+    });
+    if dest.is_none() { return }
+    let dest_index = dest.unwrap();
+    if dest_index > index { return }
+    // Empty space and coallesce
+    buffer[index].0 = None;
+    if index < buffer.len()-1 && buffer[index+1].0.is_none() {
+        buffer[index].1 += buffer[index+1].1;
+        buffer.remove(index+1);
+    }
+    if index > 0 && buffer[index-1].0.is_none() {
+        buffer[index-1].1 += buffer[index].1;
+        buffer.remove(index);
+    }
+    // Insert block
+    buffer.insert(dest_index, (block_val, block_size_needed));
+    buffer[dest_index+1].1 -= block_size_needed;
+    if buffer[dest_index+1].1 == 0 {
+        buffer.remove(dest_index+1);
+    }
 }
 
 #[named]
 fn part2() {
-    let file_contents_as_string = fs::read_to_string(&("src\\".to_string() + DAY + "\\data_test.txt")).expect("Error loading file");
-    let buffer = expand_disk(&file_contents_as_string);
-    let checksum = compute_checksum(&buffer[0..buffer.len()]);
-    let buffer2 = expand_disk_2(&file_contents_as_string);
-    print_disk_2(&buffer2);
+    let file_contents_as_string = fs::read_to_string(&("src\\".to_string() + DAY + "\\data.txt")).expect("Error loading file");
+    let mut buffer2 = expand_disk_2(&file_contents_as_string);
+    let _blocks_to_move = buffer2.iter().filter_map(|(val, _)| {
+        *val
+    }).collect::<Vec<i32>>();
+    _blocks_to_move.iter().rev().for_each(|i| {
+        try_move_block(&mut buffer2, *i);
+    });
     let checksum2 = compute_checksum_2(&buffer2);
-
-    println!("{}: {}, {}", function_name!(), checksum, checksum2);
+    
+    println!("{}: {}", function_name!(), checksum2);
 }
 
 pub fn run() {
@@ -143,4 +172,4 @@ pub fn run() {
 }
 
 // part1: 6216544403458
-// part2: 2333133121414131402
+// part2: 6237075041489
