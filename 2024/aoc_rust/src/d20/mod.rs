f848668294a0f7cf14c2b578f9daf232e36cbf8b
Jonas Keating <jonask@microsoft.com>
2025-02-10 18:27:58 -0800

    Day 20 messing about

--- 2024/aoc_rust/src/d20/mod.rs
+++ 2024/aoc_rust/src/d20/mod.rs
@@ -1,6 +1,6 @@
 use function_name::named;
 //use itertools::Itertools;
-use std::{collections::VecDeque, fmt};
+use std::fmt;
 use utilities::{Board, PointRC};
 
 const DAY: &str = "d20";
@@ -44,21 +44,23 @@ fn load_data(path: &str) -> (Board<char>, PointRC, PointRC) {
     (board, start, end)
 }
 
-fn step(board: &mut Board<MapState>, current: PointRC, end: &PointRC, path: &mut Vec<PointRC>) -> bool {
-    board[current] = MapState::Visited;
-    path.push(current);
-    if current == *end {
-        return true;
+fn find_path(board: &mut Board<MapState>, start: PointRC, end: PointRC) -> Vec<PointRC> {
+    let mut path = Vec::<PointRC>::new();
+    let mut current = start;
+    while current != end {
+        board[current] = MapState::Visited;
+        path.push(current);
+        let all_possible = current
+            .neighbors_cardinal()
+            .iter()
+            .filter(|&p| board.in_bounds(p) && board[*p] == MapState::Open)
+            .map(|&p| p.clone())
+            .collect::<Vec<PointRC>>();
+        assert!(all_possible.len() == 1);
+        current = all_possible[0];
     }
-    for next in current.neighbors_cardinal() {
-        if board.in_bounds(&next) && board[next] == MapState::Open {
-            if step(board, next, end, path) {
-                return true;
-            }
-        }
-    }
-    path.pop();
-    false
+    path.push(end);
+    path
 }
 
 #[named]
@@ -70,31 +72,17 @@ fn part1() {
         '.' | 'S' | 'E' => MapState::Open,
         _ => panic!("Unexpected char"),
     });
-    let mut path = Vec::<PointRC>::new();
-    let mut to_process = VecDeque::<PointRC>::new();
-    to_process.push_back(start);
-    while let Some(current) = to_process.pop_front() {
-        board[current] = MapState::Visited;
-        path.push(current);
-        if current == end {
-            break;
-        }
-        for next in current.neighbors_cardinal() {
-            if board.in_bounds(&next) && board[next] == MapState::Open {
-                to_process.push_back(next);
-            }
-        }
-    }
-    // let mut path2 = Vec::<PointRC>::new();
-    // let success = step(&mut board, start, &end, &mut path2);
-    // assert!(success && path == path2);
 
-    let is_cheat = |i: usize, j: usize| { 
+    let path = find_path(&mut board, start, end);
+
+    let is_cheat = |i: usize, j: usize| {
         let p: &PointRC = &path[i];
         let q: &PointRC = &path[j];
-        (p.r == q.r || p.c == q.c) && (*p).manhattan_dist(&q) == 2 && board[p.add(&q).div(2)] == MapState::Blocked
+        (p.r == q.r || p.c == q.c)
+            && (*p).manhattan_dist(&q) == 2
+            && board[p.add(&q).div(2)] == MapState::Blocked
     };
-    
+
     /* Test Data
     for diff in 3..=70 {
         let mut count = 0;
@@ -110,11 +98,11 @@ fn part1() {
         }
     }
     */
-    
-    let savings = 100;
+
+    let min_savings = 100;
     let mut count = 0;
-    for i in 0..(path.len() - savings) {
-        for j in (i + savings + 2)..path.len() {
+    for i in 0..(path.len() - min_savings) {
+        for j in (i + min_savings + 2)..path.len() {
             if is_cheat(i, j) {
                 //println!("{}: {}=>{}", diff - 2, path[i], path[j]);
                 count += 1;
