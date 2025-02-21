8be8f97cf1a500f34698d5771107b755385020fb
Jonas Keating <jonask@microsoft.com>
2025-02-21 11:20:11 -0800

    Day 22 Part 1

--- 2024/aoc_rust/src/d22/mod.rs
+++ 2024/aoc_rust/src/d22/mod.rs
@@ -13,24 +13,52 @@ mod real_data {
     pub const FILENAME: &str = r"src\d22\data.txt";
 }
 
-fn load_data(path: &str) -> Vec<String> {
+fn load_data(path: &str) -> Vec<i64> {
     let file_contents_as_string = fs::read_to_string(path).expect("Error loading file");
-    let lines = file_contents_as_string.lines().map(|s| s.to_string()).collect();
-    lines
+    file_contents_as_string.lines().map(|s| s.parse::<i64>().unwrap()).collect()
+}
+
+fn mix(secret: i64, given: i64) -> i64 {
+    secret ^ given
+}
+
+fn prune(secret: i64) -> i64 {
+    secret % 16777216
+}
+
+fn next_secret(secret: i64) -> i64 {
+    let mut next = prune(mix(secret, secret * 64));
+    next = prune(mix(next, next / 32));
+    prune(mix(next, next * 2048))
+}
+
+#[test]
+fn test_stuff() {
+    assert!(mix(42, 15) == 37);
+    assert!(prune(100000000) == 16113920);
+    assert!(next_secret(123) == 15887950);
+    assert!(next_secret(15887950) == 16495136);
 }
 
 #[named]
 fn part1() {
-    use test_data as data;
-    let lines = load_data(data::FILENAME);
-    println!("{}: {}", function_name!(), lines.len());
+    use real_data as data;
+    let numbers = load_data(data::FILENAME);
+    let final_sum = numbers.iter().map(|&n| {
+        let mut curr = n;
+        for _ in 0..2000 {
+            curr = next_secret(curr);
+        }
+        curr
+    }).sum::<i64>();
+    println!("{}: {:?}", function_name!(), final_sum);
 }
 
 #[named]
 fn part2() {
     use test_data as data;
-    let lines = load_data(data::FILENAME);
-    println!("{}: {}", function_name!(), lines.len());
+    let numbers: Vec<i64> = load_data(data::FILENAME);
+    println!("{}: {}", function_name!(), numbers.len());
 }
 
 pub fn run() {
