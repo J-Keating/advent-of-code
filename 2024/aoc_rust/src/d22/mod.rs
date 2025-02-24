cbfaad05ee35e713742d9dc0da318360168ce1be
Jonas Keating <jonask@microsoft.com>
2025-02-24 12:37:20 -0800

    Day 22 Part 2

--- 2024/aoc_rust/src/d22/mod.rs
+++ 2024/aoc_rust/src/d22/mod.rs
@@ -1,12 +1,12 @@
 use function_name::named;
-//use itertools::Itertools;
+use itertools::Itertools;
 use std::fs;
 
 const DAY: &str = "d22";
 
 #[allow(dead_code)]
 mod test_data {
-    pub const FILENAME: &str = r"src\d22\data_test.txt";
+    pub const FILENAME: &str = r"src\d22\data_test2.txt";
 }
 #[allow(dead_code)]
 mod real_data {
@@ -54,11 +54,64 @@ fn part1() {
     println!("{}: {:?}", function_name!(), final_sum);
 }
 
+fn encode(a: i32, b: i32, c: i32, d: i32) -> i32 {
+    assert!(a >= -9 && a <= 9);
+    assert!(b >= -9 && b <= 9);
+    assert!(c >= -9 && c <= 9);
+    assert!(d >= -9 && d <= 9);
+    ((a + 9) * 19 * 19 * 19) + ((b + 9) * 19 * 19) + ((c + 9) * 19) + (d + 9)
+}
+
+fn decode(n: i32) -> (i32, i32, i32, i32) {
+    let a = n / (19 * 19 * 19);
+    let b = (n / (19 * 19)) % 19;
+    let c = (n / 19) % 19;
+    let d = n % 19;
+    (a - 9, b - 9, c - 9, d - 9)
+}
+
 #[named]
 fn part2() {
-    use test_data as data;
+    use real_data as data;
+    let mut scores = Vec::new();
+    scores.resize(19*19*19*19, 0);
+    let mut seen_already = Vec::new();
+    seen_already.resize(19*19*19*19, false);
     let numbers: Vec<i64> = load_data(data::FILENAME);
-    println!("{}: {}", function_name!(), numbers.len());
+    for n in numbers.iter() {
+        seen_already.fill(false);
+        let mut curr = *n;
+        // Sequence with 2000 elements, including the start number
+        let prices = std::iter::once(curr).chain((0..2000).map(|_| {
+            curr = next_secret(curr);
+            curr
+        })).map(|s| (s % 10) as i32).collect::<Vec<i32>>();
+        // Vector with all differences
+        let diffs = prices.iter().tuple_windows().map(|(a,b)| b-a).collect::<Vec::<i32>>();
+        assert!(diffs.len() == 2000 && prices.len() == 2001);
+        // Looking at 4 diffs at a time means that the first window is at the fifth element of prices
+        let mut price_index = 4;
+        diffs.iter().tuple_windows().for_each(|(a,b,c,d)| {
+            let n = encode(*a, *b, *c, *d);
+            assert!((*a,*b,*c,*d) == decode(n));
+            if !seen_already[n as usize] {
+                scores[n as usize] += prices[price_index];
+                seen_already[n as usize] = true;
+            }
+            price_index += 1;
+        });
+//        println!("{}: {:?}", function_name!(), diffs);
+    }
+    let mut max_index = 0;
+    let mut max_score = 0;
+    scores.iter().enumerate().for_each(|(i,s)| {
+        if *s > max_score {
+            max_score = *s;
+            max_index = i;
+        }
+    });
+    let (a,b,c,d) = decode(max_index as i32);
+    println!("{}: {},{},{},{} => {}", function_name!(), a,b,c,d, max_score);
 }
 
 pub fn run() {
@@ -67,5 +120,5 @@ pub fn run() {
     part2();
 }
 
-// part1: 315
-// part2: 625108891232249
+// part1: 13234715490
+// part2: 1,-1,0,1 => 1490
