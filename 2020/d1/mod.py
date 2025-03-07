54a9ebdd9de87df563a01395527f5697d62041f9
Jonas Keating <jonask@microsoft.com>
2025-03-06 23:01:14 -0800

    use itertools

--- 2020/d1/mod.py
+++ 2020/d1/mod.py
@@ -1,5 +1,7 @@
 # Load data from real_data.txt
 
+import itertools
+
 DAY = 'd1'
 
 def load_data(file_path):
@@ -9,20 +11,20 @@ def load_data(file_path):
 
 def part1():
     data = load_data(DAY + '\\data_real.txt')
-    for n1 in data:
-        for n2 in data:
-            if n1 + n2 == 2020:
-                print(f"part1: {n1} and {n2} : '+' = {n1 + n2} : '*' = {n1 * n2}")
-                return
+    for n1, n2 in itertools.combinations(data, 2):
+        if n1 + n2 == 2020:
+            print(f"part1: {n1} and {n2} : '+' = {n1 + n2} : '*' = {n1 * n2}")
+            return
 
 def part2():
     data = load_data(DAY + '\\data_real.txt')
-    for n1 in data:
-        for n2 in data:
-            for n3 in data:
-                if n1 + n2 + n3 == 2020:
-                    print(f"part2: {n1} and {n2} and {n3} : '+' = {n1 + n2 + n3} :  '*' = {n1 * n2 * n3}")
-                    return
+    for n1, n2, n3 in itertools.combinations(data, 3):
+        if n1 + n2 + n3 == 2020:
+            print(f"part2: {n1} and {n2} and {n3} : '+' = {n1 + n2 + n3} :  '*' = {n1 * n2 * n3}")
+            return
 
 part1()
 part2()
+
+# part1: 1162 and 858 : '+' = 2020 : '*' = 996996
+# part2: 282 and 19 and 1719 : '+' = 2020 :  '*' = 9210402
