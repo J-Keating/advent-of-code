5ba0f5ae7db1f2137ee2d0bfbd072311801ee50d
Jonas Keating <jonask@microsoft.com>
2025-03-13 21:32:40 -0700

    Day 4 Part 2

--- 2020/d4/mod.py
+++ 2020/d4/mod.py
@@ -1,6 +1,4 @@
-from functools import reduce
-from operator import mul
-from typing import List, Mapping
+from typing import List
 
 DAY = 'd4'
 
@@ -30,16 +28,46 @@ def load_data(file_path) -> List[dict]:
     return ret
 
 def part1():
-    data = load_data(DAY + '\\data_test.txt')
+    data = load_data(DAY + '\\data_real.txt')
     valid_passports = []
     for passport in data:
         if all(field in passport for field in fields[:-1]):
             valid_passports.append(passport)
-    print(f"part1: {valid_passports}")
+    print(f"part1: {len(valid_passports)}")
 
+def validate_field(field: str, value: str) -> bool:
+    match field:
+        case "byr":
+            return 1920 <= int(value) <= 2002
+        case "iyr":
+            return 2010 <= int(value) <= 2020
+        case "eyr":
+            return 2020 <= int(value) <= 2030
+        case "hgt":
+            if value.endswith("cm"):
+                return 150 <= int(value[:-2]) <= 193
+            elif value.endswith("in"):
+                return 59 <= int(value[:-2]) <= 76
+            else:
+                return False
+        case "hcl":
+            return len(value) == 7 and value[0] == '#' and all(c in '0123456789abcdef' for c in value[1:])
+        case "ecl":
+            return value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        case "pid":
+            return len(value) == 9 and value.isdigit()
+        case _:
+            return True
+ 
 def part2():
-    data = load_data(DAY + '\\data_test.txt')
-    print(f"part1: {data}")    
+    data = load_data(DAY + '\\data_real.txt')
+    # def data_filter(passport):
+    #     all(validate_field(field, passport[field]) for field in fields[:-1] if field in passport)
+    valid_passports = []
+    for passport in data:
+        if all((field in passport and validate_field(field, passport[field])) for field in fields[:-1]):
+            valid_passports.append(passport)
+    print(f"part1: {len(valid_passports)}")
 
 part1()
 part2()
