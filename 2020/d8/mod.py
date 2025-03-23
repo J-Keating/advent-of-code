b53f9dcaf626f389b574f4376136fec1eb147613
Jonas Keating <jonask@microsoft.com>
2025-03-23 11:08:49 -0700

    Day 8 Part 2

--- 2020/d8/mod.py
+++ 2020/d8/mod.py
@@ -10,35 +10,48 @@ def load_data(file_path):
     lines = content.split('\n')
     return [(opcode, int(operand)) for opcode, operand in (line.split(' ') for line in lines)]
 
-def part1():
-    operations = load_data(DAY + '\\data_real.txt')
-    accumulator = 0
+def run_simulation(operations, idx_to_flip_nop_jmp): # (is_infite_loop, acc)
+    acc = 0
     ip = 0
     visited = [False] * len(operations)
     
     while ip < len(operations) and not visited[ip]:
         visited[ip] = True
-        match operations[ip][0]: 
+        (opcode, operand) = operations[ip]
+        if idx_to_flip_nop_jmp and idx_to_flip_nop_jmp == ip:
+            assert opcode == "jmp" or opcode == "nop"
+            opcode = "nop" if opcode == "jmp" else "jmp"
+
+        match opcode: 
             case "nop":
                 ip += 1
             case  "acc":
-                accumulator += operations[ip][1]
+                acc += operand
                 ip += 1
             case "jmp":
-                ip += operations[ip][1]
+                ip += operand
             case _:
                 assert False, ("Unknown Operation: " + operations[ip][0])
+    return (ip < len(operations), acc)
 
-    assert ip < len(operations)
-
-    print(f"part1: {accumulator}")
+def part1():
+    operations = load_data(DAY + '\\data_real.txt')
+    is_infinite_loop, acc = run_simulation(operations, None)
+    assert is_infinite_loop
+    print(f"part1: {acc}")
 
 def part2():
-    operations = load_data(DAY + '\\data_test.txt')
-    print(f"part2: {len(operations)}")
+    operations = load_data(DAY + '\\data_real.txt')
+    flip_indicies = [idx for idx, op in enumerate(operations) if op[0] == "nop" or op[0] == "jmp"]
+    for flip_idx in flip_indicies:
+        is_infinite_loop, acc = run_simulation(operations, flip_idx)
+        if not is_infinite_loop:
+            break
+    assert not is_infinite_loop
+    print(f"part2: {acc}")
 
 part1()
 part2()
 
 # part1: 1727
-# part2: 
+# part2: 552
