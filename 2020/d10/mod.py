cdb893a4275455585a61a0e87710706e2a350816
Jonas Keating <jonask@microsoft.com>
2025-03-28 22:43:28 -0700

    Day 10 Part 2 - fast and hacky

--- 2020/d10/mod.py
+++ 2020/d10/mod.py
@@ -1,4 +1,5 @@
 
+from email.policy import default
 import inspect
 
 DAY = 'd10'
@@ -25,16 +26,47 @@ def part1():
         diff = j - i
         assert(diff < 4)
         counts[diff] += 1
-    # print(f"{counts}")
+    #print(f"{counts}")
     print(f"{inspect.currentframe().f_code.co_name}: {counts[1] * counts[3]}")
 
 def part2():
-    data = TEST_DATA
+    data = REAL_DATA
     nums = load_data(DAY + '\\' + data.FILE_NAME)
-    print(f"{inspect.currentframe().f_code.co_name}: {len(nums)}")
+    nums.append(0)
+    nums.sort()
+    nums.append(nums[len(nums)-1] + 3)
+    diffs = [j - i for i, j in zip(nums, nums[1:])]
+    runs = []
+    curr_run_len = 0
+    curr_run_value = -1
+    for diff in diffs:
+        if diff == curr_run_value:
+            curr_run_len += 1
+        else:
+            runs.append((curr_run_value, curr_run_len))
+            curr_run_len = 1
+            curr_run_value = diff
+    #print(f"{diffs}")
+    #print(f"{runs}")
+    single_step_run_lengths = [x[1] for x in runs if x[0] == 1]
+    #print(f"{single_step_run_lengths}")
+    res = 1
+    for run_length in single_step_run_lengths:
+        match run_length:
+            case 1:
+                pass
+            case 2:
+                res *= 2
+            case 3:
+                res *= 4
+            case 4:
+                res *= 7
+            case _:
+                assert(f"Unhandled length {run_length}")
+    print(f"{inspect.currentframe().f_code.co_name}: {res}")
 
 part1()
 part2()
 
 # part1: 2030
-# part2: 
+# part2: 42313823813632
