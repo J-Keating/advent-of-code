bcf3505d3e1cbd26a64a0aa2d4753ac32441932b
Jonas Keating <jonask@microsoft.com>
2025-04-01 23:24:43 -0700

    Day 11 Part 2

--- 2020/d11/mod.py
+++ 2020/d11/mod.py
@@ -1,4 +1,5 @@
 import inspect
+import time
 
 DAY = 'd11'
 FULL_SEAT = '#'
@@ -11,6 +12,10 @@ class TEST_DATA:
 class REAL_DATA:
     FILE_NAME = 'data_real.txt'
 
+NEIGHBORS = [   (-1, -1), (0, -1), (1, -1),
+                (-1,  0),          (1,  0),
+                (-1,  1), (0,  1), (1,  1)]
+
 def load_data(file_path):
     with open(file_path, 'r') as file:
         return [list(line.strip()) for line in file]
@@ -18,18 +23,16 @@ def load_data(file_path):
 def print_array(array):
     for row in array:
         print(''.join(row))
+    print()
 
 def in_bounds(array, pos):
     return pos[0] >= 0 and pos[0] < len(array) and pos[1] >= 0 and pos[1] < len(array[0])
 
 def count_adjacent_occupied(seats, pos):
-    NEIGHBORS = [   (-1, -1), (0, -1), (1, -1),
-                    (-1,  0),          (1,  0),
-                    (-1,  1), (0,  1), (1,  1)]
     occupied = [n for n in NEIGHBORS if in_bounds(seats, (pos[0] + n[0],pos[1] + n[1])) and seats[pos[0] + n[0]][pos[1] + n[1]] == FULL_SEAT]
     return len(occupied)
 
-def new_seat_state(seats, pos):
+def new_seat_state_p1(seats, pos):
     match seats[pos[0]][pos[1]]:
         case x if x == FLOOR:
             return None
@@ -39,28 +42,65 @@ def new_seat_state(seats, pos):
             return EMPTY_SEAT if count_adjacent_occupied(seats, pos) >= 4 else None
 
 def part1():
+    start_time = time.perf_counter()
     data = REAL_DATA
-    seats = load_data(DAY + '\\' + data.FILE_NAME)
-    while True:
-        changes = []
+    new_seats = load_data(DAY + '\\' + data.FILE_NAME)
+    any_changed = True
+    while any_changed:
+        seats = [row.copy() for row in new_seats]
+        any_changed = False
         for r in range(0, len(seats)):
             for c in range(0, len(seats[0])):
-                loc = (r, c)
-                new_state = new_seat_state(seats, loc)
+                new_state = new_seat_state_p1(seats, (r,c))
                 if new_state:
-                    changes.append((loc, new_state))
-        if len(changes) == 0:
-            break
-        for loc, new_state in changes:
-            seats[loc[0]][loc[1]] = new_state
-        #print_array(seats)
+                    new_seats[r][c] = new_state
+                    any_changed = True
+        #print_array(new_seats)
     occupied = sum(len([s for s in row if s == FULL_SEAT]) for row in seats)
-    print(f"{inspect.currentframe().f_code.co_name}: {occupied}")
+    elapsed_time = time.perf_counter() - start_time
+    print(f"{inspect.currentframe().f_code.co_name} ({elapsed_time:.2f} sec) : {occupied}")
+
+def count_visible_seats(seats, pos):
+    visible_seat_count = 0
+    for n in NEIGHBORS:
+        p = (pos[0] + n[0], pos[1] + n[1])
+        while in_bounds(seats, p):
+            match seats[p[0]][p[1]]:
+                case x if x == FULL_SEAT:
+                    visible_seat_count += 1
+                    break
+                case x if x == EMPTY_SEAT:
+                    break
+            p = (p[0] + n[0], p[1] + n[1])
+    return visible_seat_count
+
+def new_seat_state_p2(seats, pos):
+    match seats[pos[0]][pos[1]]:
+        case x if x == FLOOR:
+            return None
+        case x if x == EMPTY_SEAT:
+            return FULL_SEAT if count_visible_seats(seats, pos) == 0 else None
+        case x if x == FULL_SEAT:
+            return EMPTY_SEAT if count_visible_seats(seats, pos) >= 5 else None
 
 def part2():
-    data = TEST_DATA
-    seats = load_data(DAY + '\\' + data.FILE_NAME)
-    print(f"{inspect.currentframe().f_code.co_name}: {len(seats)}")
+    start_time = time.perf_counter()
+    data = REAL_DATA
+    new_seats = load_data(DAY + '\\' + data.FILE_NAME)
+    any_changed = True
+    while any_changed:
+        seats = [row.copy() for row in new_seats]
+        any_changed = False
+        for r in range(0, len(seats)):
+            for c in range(0, len(seats[0])):
+                new_state = new_seat_state_p2(seats, (r,c))
+                if new_state:
+                    new_seats[r][c] = new_state
+                    any_changed = True
+        #print_array(new_seats)
+    occupied = sum(len([s for s in row if s == FULL_SEAT]) for row in seats)
+    elapsed_time = time.perf_counter() - start_time
+    print(f"{inspect.currentframe().f_code.co_name} ({elapsed_time:.2f} sec) : {occupied}")
 
 part1()
 part2()
