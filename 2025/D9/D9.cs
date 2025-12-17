f138e61882d2074e15594ab867ad3dd8d6727aea
Jonas Keating <jonask@microsoft.com>
2025-12-16 22:19:32 -0800

    Day 9 Part 1

--- 2025/D9/D9.cs
+++ 2025/D9/D9.cs
@@ -3,78 +3,41 @@ using System.Diagnostics;
 using System.Reflection;
 
 using AOC_Util;
-//using DataSet = AOC_Util.DataFull;
-using DataSet = AOC_Util.DataTest;
+using DataSet = AOC_Util.DataFull;
+//using DataSet = AOC_Util.DataTest;
 
-using BeamSet = System.Collections.Generic.Dictionary<int, System.Int64>;
+(Int64 row, Int64 col)[] LoadFile(string filename)
+{
+    var lines = File.ReadAllLines(filename);
+    (Int64 row, Int64 col)[] locs = lines.Select(l =>
+    {
+        var split = l.Split(',');
+        Debug.Assert(split.Length == 2);
+        return (Int64.Parse(split[1]), Int64.Parse(split[0]));
+    }).ToArray();
+    return locs;
+}
 
 void Part1(string filename)
 {
-    int totalSplits = 0;
-    var grid = FileUtil.LoadAsCharArray(filename);
-    var startLocs = GridUtil.RowData2d(grid, 0).Select((c, i) => (c, i)).Where(p => p.c == 'S').Select(p => p.i).ToArray();
-    Debug.Assert(startLocs.Length == 1);
-    var beams = new HashSet<int> { startLocs[0] };
-    for (var row = 1; row < grid.GetLength(0); row++)
+    (Int64 row, Int64 col)[] locs = LoadFile(filename);
+    Int64 maxArea = 0;
+    for (int i = 0; i < locs.Length; i++)
     {
-        var newBeams = new HashSet<int>();
-        foreach (var beam in beams)
+        for (int j = 0; j < locs.Length; j++)
         {
-            switch(grid[row, beam])
-            {
-                case '.':
-                    newBeams.Add(beam);
-                    break;
-                case '^':
-                    totalSplits++;
-                    if ((beam - 1) > 0) { newBeams.Add(beam - 1); }
-                    if ((beam + 1) < grid.GetLength(1)) { newBeams.Add(beam + 1); }
-                    break;
-                default:
-                    throw new InvalidDataException();
-            }
+            var p1 = locs[i];
+            var p2 = locs[j];
+            var area = ((Math.Abs(p1.row - p2.row) + 1) * (Math.Abs(p1.col - p2.col) + 1));
+            maxArea = Math.Max(maxArea, area);
         }
-        beams = newBeams;
     }
-    LogUtil.LogLine($"{totalSplits}");
+    LogUtil.LogLine($"{maxArea}");
 }
 
 void Part2(string filename)
 {
-    var grid = FileUtil.LoadAsCharArray(filename);
-    var startLocs = GridUtil.RowData2d(grid, 0).Select((c, i) => (c, i)).Where(p => p.c == 'S').Select(p => p.i).ToArray();
-    Debug.Assert(startLocs.Length == 1);
-    var beams = new BeamSet { { startLocs[0], 1L } };
-    for (var row = 1; row < grid.GetLength(0); row++)
-    {
-        var newBeams = new BeamSet();
-        void InsertOrAddIfInRange(int pos, Int64 count)
-        {
-            if (0 <= pos && pos < grid.GetLength(1))
-            {
-                newBeams[pos] = newBeams.TryGetValue(pos, out var currCount) ? currCount + count : count;
-            }
-        }
-
-        foreach ((int pos, Int64 count) in beams)
-        {
-            switch (grid[row, pos])
-            {
-                case '.':
-                    InsertOrAddIfInRange(pos, count);
-                    break;
-                case '^':
-                    InsertOrAddIfInRange(pos - 1, count);
-                    InsertOrAddIfInRange(pos + 1, count);
-                    break;
-                default:
-                    throw new InvalidDataException();
-            }
-        }
-        beams = newBeams;
-    }
-    Int64 totalPaths = beams.Values.Aggregate((count, acc) => acc + count);
-    LogUtil.LogLine($"{totalPaths}");
+    LogUtil.LogLine($"{filename}");
 }
 
 void Run()
@@ -92,7 +55,7 @@ public static class Config
     public static readonly string? Name = Assembly.GetExecutingAssembly()?.GetName()?.Name;
 }
 
-//D7: Part1: 1678
+//D9: Part1: 4781377701
 //completed in 6ms
-//D7: Part2: 357525737893560
-//completed in 6ms
+//D9: Part2: input.txt
+//completed in 0ms
